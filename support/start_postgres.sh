7b921862aab8f7fa2e5f722cd7a2eed596dcdd0f
TheHolyRoger <theholyroger@theholyroger.com>
2022-03-24 02:11:07 +0000

    Fix postgres tmp stats issues

---
+++ support/start_postgres.sh
@@ -0,0 +1,8 @@
+echo "Configuring postgres"
+# create temporary directory for postgres in docker
+mkdir -p /tmp/stat_temporary 
+
+# copy your postgresql.conf to postgresql config location in docker
+cp /db_config.conf /var/lib/postgresql/data/postgresql.conf
+
+echo "Done configuring."
