b48b03705c45b32bbc405d2f92e908c9f7e67369
TheHolyRoger <theholyroger@theholyroger.com>
2023-05-03 13:24:18 +0100

    Fix docker find script

--- scripts/find_docker.sh
+++ scripts/find_docker.sh
@@ -1,7 +1,7 @@
 docker_compose_bin=""
 docker_compose_version="0.0"
 docker_version="0.0"
-docker_compose_cmd=$(docker-compose --version)
+docker_compose_cmd=$(docker-compose --version 2>/dev/null)
 docker_cmd=$(docker compose version)
 if [ "${docker_compose_cmd} " != " " ]; then
     docker_compose_version_str=$(echo "$docker_compose_cmd" | awk '{split($0,a,"version "); print a[2]}' | awk '{split($0,a,","); print a[1]}')
@@ -22,7 +22,7 @@ fi
 
 compare_str="${docker_compose_version} > ${docker_version}"
 
-compare_ver=$(bc -l <<<"${compare_str}")
+compare_ver=$(bc -l <<<"${compare_str}" 2>/dev/null)
 
 if [ "$compare_ver" == "1" ]; then
     docker_compose_bin=$(which docker-compose)
